8c503d0090d842ac1b32da9c4a619e7f98bf1b0f
Will Price <pricew@southwestern.edu>
2018-12-05 22:14:27 -0600

    pandasTry now one hot encodes categorical data and scales numerical data
    I believe it works but I don't understand one hot encoding enough to be sure. A verification would be nice

--- pandasTry.py
+++ pandasTry.py
@@ -1,22 +1,47 @@
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
 import numpy
 
 #parameters
 willsFilePath = "C:\\Users\\Will\\Documents\\GitHub\\KingCreativtiy2019\\output.csv"
 elyssasFilePath = ""
-dataFile = "student-mat2.csv"
+data_file = "student-mat2.csv"
 
+#create preprocessing pipelines for both numeric and categorical data
+#https://scikit-learn.org/stable/auto_examples/compose/plot_column_transformer_mixed_types.html
+numeric_features = ['absences']
+numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median')),
+    ('scaler', StandardScaler())])
+
+categorical_features = ['school','sex','address','famsize',
+                        'Pstatus','Fedu','Mjob','Fjob',
+                        'reason','guardian','traveltime',
+                        'studytime','failures','schoolsup',
+                        'famsup','paid','activities','nursery',
+                        'higher','internet','romantic','famrel',
+                        'freetime','goout','Dalc','Walc',
+                        'health','absences']
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+
+#create a column transformer that can one hot encode categorical data and
+#median scale numerical data
+preprocessor= ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, categorical_features)])
+                        
 
 #read in the csv file
-trainingData = pd.read_csv(dataFile)
-print(trainingData)
+training_data = pd.read_csv(data_file)
 
-#One hot encode that data
-ohe = OneHotEncoder(sparse=False)
-transformed = ohe.fit_transform(trainingData)
-print(transformed)
-
-#write the csv
-df = pd.DataFrame(transformed)
-df.to_csv(willsFilePath)
+#write the processed data to a CSV 
+processed_data = preprocessor.fit_transform(training_data).toarray()[:395]
+data_frame = pd.DataFrame(processed_data)
+data_frame.to_csv(willsFilePath)
