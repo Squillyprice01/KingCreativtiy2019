052a713d57bb6ca2575d58b78b44716ac13b9b1d
Elyssa Sliheet <sliheete@southwestern.edu>
2019-02-19 09:58:08 -0600

    Update pandasTry.py

--- pandasTry.py
+++ pandasTry.py
@@ -29,7 +29,7 @@ numeric_transformer = Pipeline(steps=[
     ('scaler', StandardScaler())])
 
 #This array could be replaced with the column names of categorical data for any data set
-categorical_features = ['health','address','famsize',
+categorical_features = ['sex','address','famsize',
                         'Pstatus','Fedu','Mjob','Fjob',
                         'reason','guardian','traveltime',
                         'studytime','failures','schoolsup',
@@ -48,7 +48,8 @@ preprocessor= ColumnTransformer(
         ('num', numeric_transformer, numeric_features),
         ('cat', categorical_transformer, categorical_features)])
 
-
+#partition this data based on this column split on so that
+#male vs female two conf matrices
 ##################################  write the processed data to a CSV##################################
 #processed_data = preprocessor.fit_transform(training_data).toarray()[:training_data['school'].size]
 #data_frame = pd.DataFrame(processed_data)
@@ -69,38 +70,81 @@ pipeline = Pipeline(steps=[('preprocessor', preprocessor),
                        ('classifier', SGDClassifier(max_iter=1000000, tol= 0.000001))])
 
 #training data. Note that this attribute cannot be one of the features in the preprocessor above.
-X = training_data.drop('sex', axis=1)
+X = training_data.drop('health', axis=1)
 #training targets. Note that this attribute cannot be one of the features in the preprocessor above.
-y = training_data['sex']
+y = training_data['health']
 
+#X 75% and Y 25%
 #split data into training and testing portions for both data and targets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+print('X_test',X_test)
+MaleX_test = X_test[X_test['sex']== 'M']
+FemaleX_test = X_test[X_test['sex']== 'F']
+print('YTEST')
+print(y_test)
+
+#MaleY_test = y_test[y_test['sex']== 'M']
+#FemaleY_test = y_test[y_test['sex']== 'F']
+
+# MFTest= X_test.loc[:,'sex']
+print('MALE DATA')
+print(MaleX_test)
 
 
 #fit the data
 pipeline.fit(X_train, y_train)
-y_pred = pipeline.fit(X_train, y_train).predict(X_test)
-print(y_test)
-print(y_pred)
+y_pred = pipeline.predict(MaleX_test)
+#print('test',y_test)
+#print('pred' ,y_pred)
 
-print("model score: %.3f" % pipeline.score(X_test, y_test))
+print("model score: %.3f" % pipeline.score(MaleX_test, MaleY_test))
 # dimension of the confusion matrix is the number of unique classifiers in the training data
 # https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+def confMat(testData, predData):
+    #print('test',testData)
+    #print('predication data',predData)
+    uniqueElems = set(testData)
+    #print('uniqueElems',uniqueElems)
+    numUnique = len(uniqueElems)
+    #print('uniqueElems', uniqueElems)
+    zipping = zip(y_test,y_pred)
+    pairs = list(zipping)
+    counts = []
+    #print('TESTING',pairs)
+    for i in uniqueElems:
+        for j in uniqueElems:
+            mapped = list(map(lambda x : x[0] == i and x[1] == j, pairs))
+            #print('MAPPED',mapped)
+            count  = len(list(filter(lambda x: x, mapped)))
+            #print("i =",i,", j=",j,", count=",count)
+            counts.append(count)
+            #print('COUNTS',counts)
+
+    data = np.array(counts)
+    #print('DATA',data)
+    shape = (numUnique,numUnique)
+    #print("SHAPE",shape)
+    matrix = np.reshape(data,shape)
+    print('ELYSSA MATRIX')
+    print(matrix)
+
+confMat(y_test,y_pred)
+
 def plot_confusion_matrix(cm, classes,
                           normalize=False,
                           title='Confusion matrix',
                           cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
+
+    #This function prints and plots the confusion matrix.
+    #Normalization can be applied by setting `normalize=True`.
+
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
         print("Normalized confusion matrix")
     else:
         print('Confusion matrix, without normalization')
 
-    print(cm)
+    #print(cm)
 
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
@@ -119,24 +163,27 @@ def plot_confusion_matrix(cm, classes,
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
     plt.tight_layout()
-
+uniqueElems=set(y_test)
+unique=list(uniqueElems)
+#print(uniqueElems)
 # Compute confusion matrix
-cnf_matrix = confusion_matrix(y_test, y_pred, labels=["M", "F"])
+print('SCIKIT MATRIX')
+cnf_matrix = confusion_matrix(y_test, y_pred, labels=unique)
 np.set_printoptions(precision=2)
 
 # Plot non-normalized confusion matrix
-plt.figure()
-plot_confusion_matrix(cnf_matrix, classes='sex',
-                      title='Confusion matrix, without normalization')
+#plt.figure()
+#plot_confusion_matrix(cnf_matrix, classes=uniqueElems,
+                      #title='Confusion matrix, without normalization')
 
 # Plot normalized confusion matrix
-plt.figure()
-plot_confusion_matrix(cnf_matrix, classes='sex', normalize=True,
-                      title='Normalized confusion matrix')
+#plt.figure()
+#plot_confusion_matrix(cnf_matrix, classes=uniqueElems, normalize=True,
+                      #title='Normalized confusion matrix')
 
 print(cnf_matrix)
 #https://www.dataschool.io/simple-guide-to-confusion-matrix-terminology/
 #PrecisionFemale=
 #PrecisionMale=
-plt.show()
+#plt.show()
 #######################################################################################################
